ff51bf345d0e51c9ec9e9f9f7e2a31bbf3006148
khemmapon <khemmapon@me.com>
2017-10-25 18:51:49 +0700

    change color of Hello World text to rainbow color

--- App.jsx
+++ App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-document.body.style.backgroundColor = "steelblue";
+document.body.style.backgroundColor = 'steelblue';
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header/>
-				<Content/>
+				<Header />
+				<Content />
 			</div>
 		);
 	}
@@ -15,32 +15,38 @@ class App extends React.Component {
 
 class Header extends React.Component {
 	render() {
-		return(
+		return (
 			<div>
-				<h1 className="headerOne" style={{ color: '#27ae60', font: '70px Tahoma, Helvetica, Arial, Sans-Serif', textShadow: '0px 2px 3px rgb(114, 220, 159)' }}>Another boring '
-					<span class="amber">H</span>
-					<span class="pink">e</span>
-					<span class="yellow">l</span>
+				<h1
+					className="headerOne"
+					style={{
+						color: '#27ae60',
+						font: '70px Tahoma, Helvetica, Arial, Sans-Serif',
+						textShadow: '0px 2px 3px rgb(114, 220, 159)',
+					}}>
+					Another boring '
+					<span class="violet">H</span>
+					<span class="indigo">e</span>
 					<span class="blue">l</span>
-					<span class="green">o</span>
+					<span class="green">l</span>
+					<span class="yellow">o</span>
 					<span class=""> </span>
-					<span class="lime">W</span>
-					<span class="grey">o</span>
-					<span class="deepOrange">r</span>
-					<span class="pink">l</span>
-					<span class="amber">d</span>
-					<span class="deepOrange">!</span>
-
-
-					' app!</h1>
+					<span class="orange">W</span>
+					<span class="red">o</span>
+					<span class="orange">r</span>
+					<span class="yellow">l</span>
+					<span class="green">d</span>
+					<span class="blue">!</span>
+					' app!
+				</h1>
 			</div>
 		);
 	}
 }
 
 class Content extends React.Component {
-	render(){
-		return(
+	render() {
+		return (
 			<div id="initialDiv">
 				<h2 className="headerTwo">Can we improve it?</h2>
 				<p>Maybe we can together ...</p>
